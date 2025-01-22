3fe47af920991e8a339143bcb0704fb2402c95be
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2025-01-22 00:03:57 -0300

    feat: Atualizando a interface do quiz com Bootstrap e jQuery, implementando carregamento dinâmico de perguntas e salvamento de pontuação.

--- quiz/views.py
+++ quiz/views.py
@@ -1,7 +1,8 @@
-from django.conf import settings
 import json
 import os
 
+from django.conf import settings
+from django.core.cache import cache
 from django.http import JsonResponse
 from django.shortcuts import render
 
@@ -18,21 +19,32 @@ def index(request):
 
 
 def get_questions(request):
-    # Caminho para o arquivo JSON
-    json_path = os.path.join(settings.BASE_DIR, 'questions', 'questions.json')
-    try:
-        with open(json_path, 'r') as file:
-            questions = json.load(file)
-        return JsonResponse({"questions": questions})
-    except FileNotFoundError:
-        return JsonResponse({"error": "File not found"}, status=404)
-    except json.JSONDecodeError:
-        return JsonResponse({"error": "Invalid JSON"}, status=400)
+    questions = cache.get('questions')
+    if not questions:
+        json_path = os.path.join(
+            settings.BASE_DIR, 'questions', 'questions.json')
+        try:
+            with open(json_path, 'r') as file:
+                questions = json.load(file)
+                # Cache por 1 hora
+                cache.set('questions', questions, timeout=3600)
+        except FileNotFoundError:
+            return JsonResponse({"error": "File not found"}, status=404)
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON"}, status=400)
+    return JsonResponse({"questions": questions})
 
 
 def save_score(request):
     if request.method == "POST":
         player_name = request.POST.get("player_name")
-        points = int(request.POST.get("points"))
-        Score.objects.create(player_name=player_name, points=points)
-        return JsonResponse({"message": "Score saved successfully!"})
+        points = request.POST.get("points")
+        if not player_name or not points:
+            return JsonResponse({"error": "Invalid data"}, status=400)
+        try:
+            points = int(points)
+            score = Score.objects.create(
+                player_name=player_name, points=points)
+            return JsonResponse({"message": "Score saved successfully!", "score_id": score.id})
+        except ValueError:
+            return JsonResponse({"error": "Points must be an integer"}, status=400)
